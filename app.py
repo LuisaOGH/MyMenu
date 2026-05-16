043cc65d1336f60e814c065e8bc39922bc7fc3de
LuisaOGH <marilorsanz@gmail.com>
2026-05-16 19:01:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ df = None
 df_maestro = None
 
 # Intentar cargar archivos automáticamente desde el repositorio
-if os.path.exists("recetas_mymenu.csv") and os.path.exists("maestro_ingredients.csv"):
+if os.path.exists("recetas_mymenu.csv") and os.path.exists("maestro_ingredientes.csv"):
     df = pd.read_csv("recetas_mymenu.csv", encoding='latin1')
     df_maestro = pd.read_csv("maestro_ingredientes.csv", encoding='latin1')
     st.sidebar.success("✅ Recetas cargadas de GitHub")
